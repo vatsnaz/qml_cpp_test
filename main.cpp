f0a4760c52f7deb9656ecd0d1c148aa689ed942a
Yen-Chin Lee <coldnew.tw@gmail.com>
2014-08-10 21:56:21 +0800

    main.cpp: Register Info class to QQmlApplicationEngine rootContext
    Now we can use _info to communicate with C++ Info class.
    
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include "info.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    // Register info class in QML
+    Info m_info;
+    engine.rootContext()->setContextProperty("_info", &m_info);
+
     engine.load(QUrl(QStringLiteral("qrc:///main.qml")));
 
     return app.exec();
